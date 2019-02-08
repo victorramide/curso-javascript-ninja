609208c5ed975a50ba1b8c1f31a881550bcb45cc
Victor Ramide da Costa Souza <victorramide@gmail.com>
2019-02-08 11:18:06 -0300

    Update challenge-11.js

--- challenge-11/challenge-11.js
+++ challenge-11/challenge-11.js
@@ -1,3 +1,5 @@
+(function (){
+
 // Envolva todo o código desse arquivo em uma IIFE (incluindo esse comentário).
 
 /*
@@ -8,13 +10,22 @@ loop, mostre no console a mensagem:
 Qual loop você deve usar para que essa mensagem seja mostrada no console?
 */
 // ?
-
+  var once = false;
+  do{
+    console.log('Entrou ao menos uma vez!');
+  }while(once);
 /*
 Crie um objeto chamado `person`, que receba as seguintes propriedades:
 - 'name', 'age', 'weight' e 'birthday'. Preencha com os valores corretos
 para o nome, idade, peso e data de nascimento dessa pessoa.
 */
 // ?
+  var person = {
+	name: 'Victor',
+	age: 26,
+	weight: 70,
+	birthday: '02/10/1992'
+}
 
 /*
 Use um loop para percorrer o objeto criado acima, mostrando no console
@@ -26,7 +37,12 @@ Após o loop, mostre a frase:
 'The person has [COUNTER] properties'
 */
 // ?
-
+  var counter=0;
+  for(var prop in person){
+	  console.log('The '+prop+' of person is '+person[prop]);
+	  counter++;
+  }
+  console.log('The person has '+counter+' properties');
 /*
 Crie uma função chamada `moreThan`, que vai verificar se a pessoa (objeto
 criado acima) é mais velha que a idade passada por parâmetro.
@@ -52,3 +68,5 @@ Mostrar no console os números do array.
 */
 console.log( 'Pares de 0 a 20:' );
 // ?
+  
+}());
